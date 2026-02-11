f79e0f4669b0a72acf867f13032aacfc03d3fe5b
K NANDINI <kooranandini@gmail.com>
2026-02-11 15:05:48 +0530

    Num 5 to 9

---
+++ num5.java
@@ -0,0 +1,23 @@
+/*
+
+1    
+12   
+123  
+1234 
+12345
+
+*/
+public class num5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i >= j) {
+                    System.out.print(j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
